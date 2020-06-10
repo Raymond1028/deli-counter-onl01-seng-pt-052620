48806102c22935a98b45e779b5c608364153c6cc
Raymond Sims <lancelot53.rs@gmail.com>
2020-06-10 00:49:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ end
 
 line(katz_deli)
 
-def line_simple(array) # this one follows the same mechanics as learn.co
+def line_simple(array) 
   current_line = "The simple line is currently:"
   array.each.with_index(1) do |value, indexemus|  
   # "each.with_index" is the method...must use "index"
@@ -35,18 +35,18 @@ line_simple(katz_deli)
 
 
 def take_a_number(line, new_person)
-  line.push(new_person) # could say: "line << new_person"
+  line.push(new_person) 
   puts "Welcome, #{new_person}. You are number #{line.length} in line."
 end
 
 take_a_number(katz_deli, "Fyvish")
 
 def now_serving(line)
-  if line.length == 0 # could say: "if deli.empty?"
+  if line.length == 0 # 
     puts"There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line[0]}." # could say: "Currently serving #{line.first}."
-    line.shift # this works in the IDE but no on repl.it
+    puts "Currently serving #{line[0]}."
+    line.shift 
   end
 end
 
