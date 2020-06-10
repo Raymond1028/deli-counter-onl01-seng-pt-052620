4838a6731f6c1f179c571a541443dc6982724226
Raymond Sims <lancelot53.rs@gmail.com>
2020-06-10 00:46:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -34,18 +34,17 @@ line_simple(katz_deli)
 
 
 def take_a_number(line, new_person)
-  line.push(new_person) # could say: "line << new_person"
+  line.push(new_person) 
   puts "Welcome, #{new_person}. You are number #{line.length} in line."
 end
 
 take_a_number(katz_deli, "Fyvish")
 
 def now_serving(line)
-  if line.length == 0 # could say: "if deli.empty?"
+  if line.length == 0 
     puts"There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line[0]}." # could say: "Currently serving #{line.first}."
-    line.shift # this works in the IDE but no on repl.it
+    puts "Currently serving #{line[0]}." 
   end
 end
 
